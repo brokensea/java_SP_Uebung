3d8fa68ac4dec66f1016f07ea8123d3d15a56628
lalagum <maksmadeng@gmail.com>
2024-06-25 16:52:48 +0200

    fertig

--- DAY2_25_6_2024/VariablenUndDatenstrukturen/BerechnungVonAusdruecken.java
+++ DAY2_25_6_2024/VariablenUndDatenstrukturen/BerechnungVonAusdruecken.java
@@ -1,5 +1,46 @@
 package DAY2_25_6_2024.VariablenUndDatenstrukturen;
 
 public class BerechnungVonAusdruecken {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 6;
+        double x = 1.5;
+        double y = 2.3;
+        int int_ergebnis;
+        double double_ergebnis;
 
+        int_ergebnis = c - a * 3; // -3
+        System.out.println(int_ergebnis);
+        int_ergebnis = c / a; // 2
+        System.out.println(int_ergebnis);
+        int_ergebnis = c / b; // 1
+        System.out.println(int_ergebnis);
+        int_ergebnis = a + b / c; // 3
+        System.out.println(int_ergebnis);
+        // int a = 5;
+        // int b = a++; // b = 5, a = 6
+        int_ergebnis = a++; // 3
+        System.out.println(int_ergebnis);
+        // int a = 5;
+        // int b = ++a; // b = 6, a = 6
+        int_ergebnis = ++a; // 5
+        System.out.println(int_ergebnis);
+        int_ergebnis = b + a--; // 10
+        System.out.println(int_ergebnis);
+        int_ergebnis = b + --a; // 8
+        System.out.println(int_ergebnis);
+        // int_ergebnis = a**; //error
+        double_ergebnis = c / b; // 1.0
+        System.out.println(double_ergebnis);
+        double_ergebnis = x + y * b; // 13.0
+        System.out.println(double_ergebnis);
+        double_ergebnis = (x + y) * b; // 19.0
+        System.out.println(double_ergebnis);
+        double_ergebnis = c + a / b; // 6.0
+        System.out.println(double_ergebnis);
+        double_ergebnis = (x * c - a); // 6.0
+        System.out.println(double_ergebnis);
+
+    }
 }
--- DAY2_25_6_2024/VariablenUndDatenstrukturen/DatentypenMiteinanderVerrechne.java
+++ DAY2_25_6_2024/VariablenUndDatenstrukturen/DatentypenMiteinanderVerrechne.java
@@ -1,5 +1,31 @@
 package DAY2_25_6_2024.VariablenUndDatenstrukturen;
 
 public class DatentypenMiteinanderVerrechne {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 5.5;
+        char c = 'A';
+        String text = "Hallo";
 
+        // 1. Addieren von a, b und c und Zuweisen des Ergebnisses zu
+        // einer double-Variable result1
+        double result1 = a + b + c;
+        System.out.println(result1);
+
+        // 2. Multiplizieren von a und c und Zuweisen des Ergebnisses zu einer
+        // int-Variable result2
+        int result2 = a * c;
+        System.out.println(result2);
+
+        // 3. Dividieren von b durch c und Zuweisen des Ergebnisses zu einer
+        // double-Variable result3
+        double result3 = b / c;
+        System.out.println(result3);
+
+        // 4. Konkatenieren von text und c (als String) und Zuweisen des
+        // Ergebnisses zu einer String-Variable result4
+        String result4 = "text" + c;
+        System.out.println(result4);
+
+    }
 }
