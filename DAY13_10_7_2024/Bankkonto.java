509eb5f2725f1cc37deff04ab3477e2adf23d653
lalagum <maksmadeng@gmail.com>
2024-07-09 21:21:32 +0200

    OPP Bankkonto

--- DAY13_10_7_2024/Bankkonto.java
+++ DAY13_10_7_2024/Bankkonto.java
@@ -1,5 +1,13 @@
 package DAY13_10_7_2024;
 
+import java.time.LocalDate;
+// import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+// import java.time.LocalDateTime;
+// import java.time.Year;
+import java.util.ArrayList;
+
 // Implementiere eine Java-Klasse namens**`Bankkonto`**,die die folgenden Attribute hat:
 
 // -**`kontoinhaber`**(String):Der Name des Kontoinhabers-**`kontonummer`**(String):Die Kontonummer des Bankkontos-**`kontostand`**(double):Der aktuelle Kontostand
@@ -16,5 +24,130 @@ public class Bankkonto {
     private String kontoinhaber;
     private String kontonummer;
     private double kontostand;
+    private ArrayList<Transaktion> transaktionshistorien;
+    // kann mann mit Array mit clone machen;
+
+    public Bankkonto(String kontoinhaber, String kontonummer) {
+        this.setKontoinhaber(kontoinhaber);
+        this.setKontonummer(kontonummer);
+        this.setKontostand(0.0);
+        this.transaktionshistorien = new ArrayList<>();
+    }
+
+    public String getKontoinhaber() {
+        return kontoinhaber;
+    }
+
+    public void setKontoinhaber(String kontoinhaber) {
+        this.kontoinhaber = kontoinhaber;
+    }
+
+    public String getKontonummer() {
+        return kontonummer;
+    }
+
+    public void setKontonummer(String kontonummer) {
+        this.kontonummer = kontonummer;
+    }
+
+    public double getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(double kontostand) {
+        this.kontostand = kontostand;
+    }
+
+    // einzahlen(double betrag): Erhöht den Kontostand um den übergebenen Betrag
+    public boolean einzahlen(double betrag) {
+        if (betrag > 0) {
+            this.kontostand += betrag;
+            Transaktion tr = new Transaktion(Transaktionstyp.Einzahlung, betrag);
+            transaktionshistorien.add(tr);
+            return true;
+        }
+        System.out.println("Unzureichender Betrag, fehlgeschlagen");
+        return false;
+    }
+
+    // abheben(double betrag): Verringert den Kontostand um den übergebenen Betrag,
+    // wenn ausreichend Guthaben vorhanden ist
+    public boolean abheben(double betrag) {
+        if (betrag > this.kontostand) {
+            System.out.println("Unzureichender Kontostand, fehlgeschlagen");
+            return false;
+        }
+        Transaktion tr = new Transaktion(Transaktionstyp.Abhebung, betrag);
+        transaktionshistorien.add(tr);
+        this.kontostand -= betrag;
+        return true;
+    }
+
+    // gibKontoinformationen(): Gibt die Kontoinformationen (Kontoinhaber,
+    // Kontonummer und Kontostand) auf der Konsole aus
+    public void gibKontoinformationen() {
+        System.out.println(this.toString());
+    }
+
+    public boolean ueberweisen(Bankkonto empfaenger, double betrag) {
+        if (this.abheben(betrag) && empfaenger.einzahlen(betrag)) {
+            System.out.println(empfaenger.kontoinhaber + " erhält " + betrag + " von " + this.kontoinhaber);
+            return true;
+        } else {
+            System.out.println(empfaenger.kontoinhaber + " erhält NICHT " + betrag + " von " + this.kontoinhaber);
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bankkonto{" +
+                "kontoinhaber='" + kontoinhaber + '\'' +
+                ", kontonummer='" + kontonummer + '\'' +
+                ", kontostand=" + kontostand +
+                '}';
+    }
+
+    public void printTransaktionshistorien() {
+        System.out.println();
+        System.out.println("Transaktionshistorien von " + this.kontoinhaber + "ist: ");
+        for (Transaktion aTransaktion : this.transaktionshistorien) {
+            System.out.println(aTransaktion.toString());
+        }
+    }
+
+    enum Transaktionstyp {
+        Einzahlung,
+        Abhebung,
+        Ueberweisung
+    }
+
+    private class Transaktion {
+        private Transaktionstyp transaktionstyp;
+        private double betrag;
+        private LocalDate date;
+        private LocalTime time;
+
+        public Transaktion(Transaktionstyp transaktionstyp, double betrag) {
+            this.transaktionstyp = transaktionstyp;
+            this.betrag = betrag;
+            this.date = LocalDate.now();
+            this.time = LocalTime.now();
+        }
+
+        @Override
+        public String toString() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+            return "Transaktion{" +
+                    "transaktionstyp=" + transaktionstyp +
+                    ", betrag=" + betrag +
+                    ", date=" + date.format(dateFormatter) +
+                    ", time=" + time.format(timeFormatter) +
+                    '}';
+        }
+
+    }
 
 }
---
+++ DAY13_10_7_2024/BankkontoTest.java
@@ -0,0 +1,55 @@
+package DAY13_10_7_2024;
+
+public class BankkontoTest {
+    public static void main(String[] args) {
+        initTest();
+    }
+
+    private static void initTest() {
+        Bankkonto konto1 = new Bankkonto("Max C", "123");
+        Bankkonto konto2 = new Bankkonto("Erika M", "456");
+        Bankkonto konto3 = new Bankkonto("John D", "789");
+        Bankkonto konto4 = new Bankkonto("Jane E", "100");
+        Bankkonto konto5 = new Bankkonto("Alice A", "200");
+
+        // Einzahlungen
+        konto1.einzahlen(1000);
+        konto2.einzahlen(2000);
+        konto3.einzahlen(3000);
+        konto4.einzahlen(4000);
+        konto5.einzahlen(5000);
+
+        // Abhebungen
+        konto1.abheben(500);
+        konto2.abheben(1000);
+        konto3.abheben(1500);
+        konto4.abheben(2000);
+        konto5.abheben(2500);
+
+        // Überweisungen
+        konto1.ueberweisen(konto2, 100);
+        konto3.ueberweisen(konto4, 200);
+        konto5.ueberweisen(konto1, 300);
+
+        // Print
+        konto1.gibKontoinformationen();
+        konto1.printTransaktionshistorien();
+        System.out.println();
+
+        konto2.gibKontoinformationen();
+        konto2.printTransaktionshistorien();
+        System.out.println();
+
+        konto3.gibKontoinformationen();
+        konto3.printTransaktionshistorien();
+        System.out.println();
+
+        konto4.gibKontoinformationen();
+        konto4.printTransaktionshistorien();
+        System.out.println();
+
+        konto5.gibKontoinformationen();
+        konto5.printTransaktionshistorien();
+    }
+
+}
